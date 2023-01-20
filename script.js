f432e7fb5208137363f8164b73d421b21a1cd53f
Gustavo Alvarenga <gustavo.alvarenga287@gmail.com>
2023-01-20 13:53:20 -0300

    Includi Info e Form

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function proximaImagem() {
 
   document.getElementById("radio" + contador).checked = true
 }
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const telefoneInput = document.querySelector("#telefone");
+const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("#submit", (event) =>{
+  event.preventDefault()
+
+  //Verifica se o nome está vazio
+  if(nameInput.value === ""){
+    alert("Por favor, preencha seu nome!")
+    return
+  }
+  //Verificar email
+  if(emailInput.value === ""){
+    alert("Por favor, preencha o seu email!")
+    return
+  }
+  //Verifica telefone
+  if (telefoneInput.value === "") {
+  alert("Por favor, preencha o seu telefone!")
+  return
+  }
+  //Verifica mensagem
+  if (messageTextarea.value === "") {
+  alert("Por favor, digite a mensagem!")
+  return  
+  } 
+  //Envia todos os campos
+  form.submit()
+
+});
+
+
+
+
