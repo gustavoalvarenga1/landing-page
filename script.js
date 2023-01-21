3eba27349e594cf997c5970c0223c39defaf6cf4
Gustavo Alvarenga <gustavo.alvarenga287@gmail.com>
2023-01-21 11:14:34 -0300

    Validação form e scroll

--- script.js
+++ script.js
@@ -4,49 +4,33 @@ document.getElementById("radio1").checked = true
 setInterval(function () {
   proximaImagem()
 }, 3500)
-
 function proximaImagem() {
   contador++
   if (contador > 4) {
     contador = 1
   }
-
   document.getElementById("radio" + contador).checked = true
 }
-const form = document.querySelector("#form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const telefoneInput = document.querySelector("#telefone");
-const messageTextarea = document.querySelector("#message");
 
-form.addEventListener("#submit", (event) =>{
-  event.preventDefault()
 
-  //Verifica se o nome está vazio
-  if(nameInput.value === ""){
+function validar() {
+  if (document.getElementById("name").value === "") {
     alert("Por favor, preencha seu nome!")
-    return
   }
   //Verificar email
-  if(emailInput.value === ""){
+  if (document.getElementById("email").value === "") {
     alert("Por favor, preencha o seu email!")
-    return
   }
   //Verifica telefone
-  if (telefoneInput.value === "") {
-  alert("Por favor, preencha o seu telefone!")
-  return
+  if (document.getElementById("telefone").value === "") {
+    alert("Por favor, preencha o seu telefone!")
   }
   //Verifica mensagem
-  if (messageTextarea.value === "") {
-  alert("Por favor, digite a mensagem!")
-  return  
-  } 
-  //Envia todos os campos
-  form.submit()
-
-});
-
-
+  if (document.getElementById("mensagem").value === "") {
+    alert("Por favor, digite a mensagem!")
+  }else{
+    alert("Mensagem enviada!")
+  }
+}
 
 
